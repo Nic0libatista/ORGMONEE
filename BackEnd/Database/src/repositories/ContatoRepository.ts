adbf2f5ff51691149e27c8a9e26a44952d50d7fd
Gabriela Queiroz <gabrielaqueiroznunes2501@gmail.com>
2025-07-30 17:27:18 -0300

    To tentando fazer a parte de cadastro e login de usuario

---
+++ BackEnd/Database/src/repositories/ContatoRepository.ts
@@ -0,0 +1,46 @@
+import { resolve } from "path";
+import Contato from "../classes/Contato";
+import Commands from "../interfaces/Commands";
+import { rejects } from "assert";
+import { conexao } from "../database/Config";
+import { error } from "console";
+
+export default class ContatoRepository implements Commands<Contato>{
+    Cadastrar(obj: Contato): Promise<Contato> {
+        return new Promise((resolve,reject)=>{
+            conexao.query("INSERT INTO Contato(telefone_comercial, telefone_residencial, telefone_celular, email) VALUES (?,?,?)",[
+                obj.telefone_celular,
+                obj.telefone_comercial,
+                obj.telefone_residencial
+            ],
+        (erro, end:any)=>{
+            if (erro) {
+                return reject(erro)
+            } else {
+                return resolve(obj)
+            }
+        })
+        });
+    }
+    Listar(): Promise<Contato[]> {
+        return new Promise((resolve,reject)=>{
+            conexao.query("Select * from Contato", (erro, result)=>{
+                if (erro) {
+                    return reject(erro)
+                } else {
+                    return resolve(result as Contato [])
+                }
+            })
+        });
+    }
+    Apagar(id: number): Promise<string> {
+        throw new Error("Method not implemented.");
+    }
+    Atualizar(obj: Contato): Promise<Contato> {
+        throw new Error("Method not implemented.");
+    }
+    PesquisarId(id: number): Promise<Contato> {
+        throw new Error("Method not implemented.");
+    }
+    
+}
